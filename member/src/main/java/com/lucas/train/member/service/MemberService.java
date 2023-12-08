30c4e82055156ab8eaca670e25772c2cb5afe7dc
LuuucasChen <1120565776@qq.com>
2023-12-08 13:13:21 +0800

    实现简单的注册接口

--- member/src/main/java/com/lucas/train/member/service/MemberService.java
+++ member/src/main/java/com/lucas/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.lucas.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.lucas.train.member.domain.Member;
+import com.lucas.train.member.domain.MemberExample;
 import com.lucas.train.member.mapper.MemberMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -11,5 +16,23 @@ public class MemberService {
 
     public int count() {
         return (int) memberMapper.countByExample(null);
+    }
+
+    public long register(String mobile) {
+        MemberExample memberExample = new MemberExample();
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+
+        if (CollUtil.isNotEmpty(list)) {
+//            return list.get(0).getId();
+            throw new RuntimeException("手机号已注册");
+        }
+
+        Member member = new Member();
+        member.setId(System.currentTimeMillis());
+        member.setMobile(mobile);
+
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
