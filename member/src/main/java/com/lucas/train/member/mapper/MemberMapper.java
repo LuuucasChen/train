67eefedb645355672db2209b1b9e7c13f9d2738d
LuuucasChen <1120565776@qq.com>
2023-12-08 12:55:07 +0800

    集成mybatis官方代码生成器

--- member/src/main/java/com/lucas/train/member/mapper/MemberMapper.java
+++ member/src/main/java/com/lucas/train/member/mapper/MemberMapper.java
@@ -1,8 +1,33 @@
 package com.lucas.train.member.mapper;
 
+import com.lucas.train.member.domain.Member;
+import com.lucas.train.member.domain.MemberExample;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface MemberMapper {
-    public int count();
+    long countByExample(MemberExample example);
+
+    int deleteByExample(MemberExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Member record);
+
+    int insertSelective(Member record);
+
+    List<Member> selectByExample(MemberExample example);
+
+    Member selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByExample(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByPrimaryKeySelective(Member record);
+
+    int updateByPrimaryKey(Member record);
 }
