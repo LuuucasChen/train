7d8a1d2ad4e60923ec3a4b5876a38443d0924a41
LuuucasChen <1120565776@qq.com>
2024-01-24 00:31:33 +0800

    修改quartz配置方式

--- batch/src/main/java/com/lucas/train/batch/config/QuartzConfig.java
+++ batch/src/main/java/com/lucas/train/batch/config/QuartzConfig.java
@@ -1,36 +1,36 @@
-package com.lucas.train.batch.config;
-
-import com.lucas.train.batch.job.QuartzTestJob;
-import org.quartz.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class QuartzConfig {
-
-    /**
-     * 声明一个任务
-     * @return
-     */
-    @Bean
-    public JobDetail jobDetail() {
-        return JobBuilder.newJob(QuartzTestJob.class)
-                .withIdentity("TestJob", "test")
-                .storeDurably()
-                .build();
-    }
-
-    /**
-     * 声明一个触发器，什么时候触发这个任务
-     * @return
-     */
-    @Bean
-    public Trigger trigger() {
-        return TriggerBuilder.newTrigger()
-                .forJob(jobDetail())
-                .withIdentity("trigger", "trigger")
-                .startNow()
-                .withSchedule(CronScheduleBuilder.cronSchedule("*/5 * * * * ?"))
-                .build();
-    }
-}
+//package com.lucas.train.batch.config;
+//
+//import com.lucas.train.batch.job.QuartzTestJob;
+//import org.quartz.*;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class QuartzConfig {
+//
+//    /**
+//     * 声明一个任务
+//     * @return
+//     */
+//    @Bean
+//    public JobDetail jobDetail() {
+//        return JobBuilder.newJob(QuartzTestJob.class)
+//                .withIdentity("TestJob", "test")
+//                .storeDurably()
+//                .build();
+//    }
+//
+//    /**
+//     * 声明一个触发器，什么时候触发这个任务
+//     * @return
+//     */
+//    @Bean
+//    public Trigger trigger() {
+//        return TriggerBuilder.newTrigger()
+//                .forJob(jobDetail())
+//                .withIdentity("trigger", "trigger")
+//                .startNow()
+//                .withSchedule(CronScheduleBuilder.cronSchedule("*/5 * * * * ?"))
+//                .build();
+//    }
+//}
--- batch/src/main/java/com/lucas/train/batch/job/QuartzTestJob.java
+++ batch/src/main/java/com/lucas/train/batch/job/QuartzTestJob.java
@@ -1,9 +1,11 @@
 package com.lucas.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution
 public class QuartzTestJob implements Job {
 
     @Override
