f21bfd32f630d59ae2fe79aa9778941f4a58a479
LuuucasChen <1120565776@qq.com>
2024-01-24 19:23:14 +0800

    集成openfeign实现服务间调用

--- batch/src/main/java/com/lucas/train/batch/controller/TestController.java
+++ batch/src/main/java/com/lucas/train/batch/controller/TestController.java
@@ -1,12 +1,19 @@
 package com.lucas.train.batch.controller;
 
+import com.lucas.train.batch.feign.BusinessFeign;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Autowired
+    private BusinessFeign businessFeign;
+
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World !! Batch";
+        String hello = businessFeign.hello();
+        System.out.print(hello);
+        return hello + " Hello World !! Batch";
     }
 }
